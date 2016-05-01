c88c7c92d6a5b28cb75320e2423ae96dc25cc74a
Suwijak Chaipipat <ppnk2537@gmail.com>
2016-05-01 22:52:14 +0700

    Initial Commit

---
+++ ticket-provider-ios/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ticket-provider-ios
+//
+//  Created by Suwijak Chaipipat on 5/1/2559 BE.
+//  Copyright © 2559 Suwijak Chaipipat. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
