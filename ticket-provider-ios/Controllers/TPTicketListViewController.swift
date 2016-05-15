e93bb8cc05d651da082dae366502ca4604a5c3c3
Suwijak Chaipipat <ppnk2537@gmail.com>
2016-05-15 14:12:29 +0700

    [#118816665] Create uitableview and bind with API

--- ticket-provider-ios/Controllers/TPTicketListViewController.swift
+++ ticket-provider-ios/Controllers/TPTicketListViewController.swift
@@ -7,18 +7,57 @@
 //
 
 import UIKit
+import SwiftyJSON
 
-class TPTicketListViewController: UIViewController {
+class TPTicketListViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    
+    var tickets: JSON?
+    let textCellIdentifier = "TPticketListTableViewCell"
+    
+    @IBOutlet weak var ticketListTableView: UITableView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         self.navigationController?.navigationBar.hidden = false
         
-        print("ticket-list")
+        self.ticketListTableView.delegate = self
+        self.ticketListTableView.dataSource = self
+        
+        TPHttpManager.sharedInstance.retrieveTickets({
+            responseCode, data in
+                if responseCode == 200 {
+                    self.tickets = data["tickets"]
+                    self.ticketListTableView.reloadData()
+                }
+            }, errorBlock: {
+                
+        })
     }
     
     override func viewWillAppear(animated: Bool) {
         self.addLeftBarButtonWithImage(UIImage(named: "HamburgerIcon")!)
+    }
+    
+    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        guard let tickets = self.tickets else {
+            return 0
+        }
+        return tickets.count
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier(self.textCellIdentifier, forIndexPath: indexPath)
+        
+        let row = indexPath.row
+        if let ticket = self.tickets?[row] {
+            cell.textLabel?.text = "\(ticket["activity_name"])"
+        }
+        
+        return cell
     }
 }
